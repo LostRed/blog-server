e90742ad55a3b50a7b3a14862d548adcd47d3818
Luwei Deng <lostred@outlook.com>
2021-01-18 11:02:48 +0800

    update

--- db/blog-data.sql
+++ db/blog-data.sql
@@ -7,35 +7,36 @@
 /*==============================================================*/
 /* Table: admin                                                 */
 /*==============================================================*/
-insert into admin (status_id, username, password, gmt_create, gmt_modified, deleted)
-values (1, 'lostred', '123456', now(), now(), 0);
+INSERT INTO `admin` (`id`, `status_id`, `username`, `password`, `gmt_create`, `gmt_modified`, `deleted`)
+VALUES (1, 1, 'lostred', '123456', now(), now(), 0);
 
 /*==============================================================*/
 /* Table: article                                               */
 /*==============================================================*/
-insert into article (status_id, user_id, catalogue_id, title, cover, content, hot, gmt_create, gmt_modified, deleted)
-values (1, 1, 1, '测试文章1', 'url路径', '这是一个测试内容文字', 0, now(), now(), 0);
+INSERT INTO `article` (`id`, `status_id`, `user_id`, `catalogue_id`, `title`, `cover`, `content`, `hot`, `gmt_create`,
+                       `gmt_modified`, `deleted`)
+VALUES (1, 1, 1, 1, '测试文章1', 'url路径', '这是一个测试内容文字', 0, now(), now(), 0);
 
 /*==============================================================*/
 /* Table: catalogue                                             */
 /*==============================================================*/
-insert into catalogue (status_id, name)
-values (1, 'Java');
-insert into catalogue (status_id, name)
-values (1, '音乐');
-insert into catalogue (status_id, name)
-values (1, '诗词');
-
+INSERT INTO `catalogue` (`id`, `status_id`, `name`)
+VALUES (1, 1, 'Java');
+INSERT INTO `catalogue` (`id`, `status_id`, `name`)
+VALUES (2, 1, '音乐');
+INSERT INTO `catalogue` (`id`, `status_id`, `name`)
+VALUES (3, 1, '诗词');
 /*==============================================================*/
 /* Table: status                                                */
 /*==============================================================*/
-insert into status (name, type, value)
-values ('启用', '使用状态', 1);
-insert into status (name, type, value)
-values ('禁用', '使用状态', 0);
+INSERT INTO `status` (`id`, `name`, `type`, `value`)
+VALUES (1, '启用', '使用状态', 1);
+INSERT INTO `status` (`id`, `name`, `type`, `value`)
+VALUES (2, '禁用', '使用状态', 0);
 
 /*==============================================================*/
 /* Table: user                                                  */
 /*==============================================================*/
-insert into user (status_id, username, password, name, sex, email, gmt_create, gmt_modified, deleted)
-values (1, 'lostred', '123456', '邓路炜', 1, 'lostred@outlook.com', now(), now(), 0);
+INSERT INTO `user` (`id`, `status_id`, `username`, `password`, `avatar`, `name`, `sex`, `email`, `gmt_create`,
+                    `gmt_modified`, `deleted`)
+VALUES (1, 1, 'lostred', '123456', 'url', '邓路炜', 1, 'lostred@outlook.com', now(), now(), 0);
